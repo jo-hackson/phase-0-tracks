fbb0bd7cdca1966313c56e7a393853e344373d26
jo-hackson <joanna.yeh.jackson@gmail.com>
2017-05-13 14:04:39 +0100

    Complete 5.1 with Ruby methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,43 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+# p "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+# p "zom".insert(1, "o")
 # => “zoom”
 
-# "enhance".<???>
+# p "enhance".center(20)
+# p "enhance".center(20, " ")
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+# p "Stop! You’re under arrest!".upcase
+# "Stop! You’re under arrest!".each_char { |c| print c.upcase}
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
-#=> "the usual suspects"
-
-# " suspects".<???>
+# p "the usual".insert(-1, " suspects")
+# p "the usual".concat(" suspects")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+# p " suspects".prepend("the usual")
+# => "the usual suspects"
+
+# p "The case of the disappearing last letter".chop
+# p "The case of the disappearing last letter".delete "r"
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+# p "The case of the disappearing first letter".delete "T"
+# p "The case of the disappearing first letter".reverse.chop.reverse
+
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+# p "Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+# "z".each_byte {|c| print c, ' '}
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+# p "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
+
