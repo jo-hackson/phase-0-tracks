7ee49794f2ad53b261e61f5ba38e9ca851779272
jo-hackson <joanna.yeh.jackson@gmail.com>
2017-05-15 23:26:18 +0100

    Complete and confirm that change_letter method works; method will change the letter to the next relevant letter

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,25 +1,35 @@
-def change_letter(char)
-	char = char.downcase
-	vowel = "aeiou"
-	consonant = "bcdfghjklmnpqrstvwxyz"
+puts "Enter a full name: "
+name_input = gets.chomp
 
-	if char == " "
-		" "
-	elsif char == "z"
-		"a"
-	elsif vowel.index(char) == nil && consonant.index(char) == nil
-		char
-	elsif vowel.index(char) == nil
-	# if the char does not match with one of the values in vowel
-		con_position = consonant.index(char).next
-		new_con = consonant[con_position]
-		new_con
-		# puts "con"
-	else
-		vowel_position = vowel.index(char).next
-		new_vowel = vowel[vowel_position]
-		new_vowel
-		# puts "vow"
+def alias_maker(name_input)
+
+# method to change the letter depending on
+# if it is a vowel or consonant
+# input a character
+# output the next character
+	def change_letter(char)
+		char = char.downcase
+		vowel = "aeiou"
+		consonant = "bcdfghjklmnpqrstvwxyz"
+
+		if char == " "
+			" "
+		elsif char == "z"
+			"a"
+		elsif vowel.index(char) == nil && consonant.index(char) == nil
+			char
+		elsif vowel.index(char) == nil
+		# if the char does not match with one of the values in vowel
+			con_position = consonant.index(char).next
+			new_con = consonant[con_position]
+			new_con
+			# puts "con"
+		else
+			vowel_position = vowel.index(char).next
+			new_vowel = vowel[vowel_position]
+			new_vowel
+			# puts "vow"
+		end
 	end
-end
 
+end
