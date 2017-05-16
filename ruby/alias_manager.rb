e68cca116271e90bce3e59f57d282dcdf132b8af
jo-hackson <joanna.yeh.jackson@gmail.com>
2017-05-16 09:54:00 +0100

    Work on 5.5 solo challenge, not so hot

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,35 +1,84 @@
 puts "Enter a full name: "
-name_input = gets.chomp
+original_name = gets.chomp
 
-def alias_maker(name_input)
+# def alias_maker
 
-# method to change the letter depending on
-# if it is a vowel or consonant
-# input a character
-# output the next character
-	def change_letter(char)
-		char = char.downcase
-		vowel = "aeiou"
-		consonant = "bcdfghjklmnpqrstvwxyz"
-
-		if char == " "
-			" "
-		elsif char == "z"
-			"a"
-		elsif vowel.index(char) == nil && consonant.index(char) == nil
-			char
-		elsif vowel.index(char) == nil
-		# if the char does not match with one of the values in vowel
-			con_position = consonant.index(char).next
-			new_con = consonant[con_position]
-			new_con
-			# puts "con"
-		else
-			vowel_position = vowel.index(char).next
-			new_vowel = vowel[vowel_position]
-			new_vowel
-			# puts "vow"
-		end
+	def swap_names_positions(original_name)
+		split_name = original_name.split(" ")
+		
+		# swap first and last name positions
+		split_name[0], split_name[1] = split_name[1], split_name[0]
+		swap_name = split_name[0], split_name[1]
+		swap_name
 	end
 
-end
+
+
+# end
+
+p swap_names_positions(original_name)
+
+
+
+
+# # # this will separate the letters 
+# 	def separate_characters
+# 		puts "hello"
+# 		index = 0
+# 		# container response, add stuff as we go
+# 		conversion = ""
+# 		while index < original_name.length
+# 			# conversion is getting built as the program runs
+# 			conversion << @swap_name(original_name[index]).to_s
+# 			index += 1
+# 		end
+# 		conversion
+# 	end
+
+# p separate_characters(@swap_name)
+# print separate_characters(@swap_name)
+# puts separate_characters(@swap_name)
+
+
+# # method to change the letter depending on
+# # if it is a vowel or consonant
+# # input a character
+# # output the next character
+# 	def change_letter(char)
+# 		char = char.downcase
+# 		vowel = "aeiou"
+# 		consonant = "bcdfghjklmnpqrstvwxyz"
+
+# 		if char == " "
+# 			" "
+# 		elsif char == "z"
+# 			"a"
+# 		elsif vowel.index(char) == nil && consonant.index(char) == nil
+# 			char
+# 		elsif vowel.index(char) == nil
+# 		# if the char does not match with one of the values in vowel
+# 			con_position = consonant.index(char).next
+# 			new_con = consonant[con_position]
+# 			new_con
+# 		else
+# 			vowel_position = vowel.index(char).next
+# 			new_vowel = vowel[vowel_position]
+# 			new_vowel
+# 		end
+
+# 	end # end of change_letter method
+
+
+
+
+
+# end
+
+
+
+
+
+
+
+
+# end # end of alias_maker method
