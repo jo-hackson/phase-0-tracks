ed87cad3108b7aa7a96e586899e23de35fae14b4
jo-hackson <joanna.yeh.jackson@gmail.com>
2017-05-16 12:49:51 +0100

    Start work on 5.7

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -5,11 +5,14 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 
 # 1. Iterate through the zombie_apocalypse_supplies array,
 # printing each item in the array separated by an asterisk
-# ----
+# zombie_apocalypse_supplies.each {|x| print x, " * "}
 
 # 2. In order to keep yourself organized, sort your zombie_apocalypse_supplies
 # in alphabetical order. Do not use any special built-in methods.
-# ----
+# p zombie_apocalypse_supplies[-1], zombie_apocalypse_supplies[3], 
+# zombie_apocalypse_supplies[-2], zombie_apocalypse_supplies[-3], 
+# zombie_apocalypse_supplies[0,],  zombie_apocalypse_supplies[1],
+# zombie_apocalypse_supplies[-4], zombie_apocalypse_supplies[2]
 
 # 3. Create a method to see if a particular item (string) is in the
 # zombie_apocalypse_supplies. Do not use any special built-in methods.
@@ -28,7 +31,9 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 # documentation for Arrays.
 other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
                             "solar battery", "flashlight"]
-# ----
+
+# joint_supplies = (zombie_apocalypse_supplies + other_survivor_supplies).uniq 
+# p joint_supplies
 
 # Hash Drills
 
@@ -44,17 +49,18 @@ extinct_animals = {
 
 # 1. Iterate through extinct_animals hash, printing each key/value pair
 # with a dash in between the key and value, and an asterisk between each pair.
-# ----
+# extinct_animals.each {|key, value| print " #{key}--#{value} *"}
 
 # 2. Keep only animals in extinct_animals if they were extinct before
 # the year 2000. Do not use any special built-in methods.
-# ----
+
 
 # 3. Our calculations were completely off, turns out all of those animals went
 # extinct 3 years before the date provided. Update the values in extinct_animals
 # so they accurately reflect what year the animal went extinct.
 # Do not use any special built-in methods.
 # ----
+
 
 # 4. You've heard that the following animals might be extinct, but you're not sure.
 # Check if they're included in extinct_animals, one by one:
@@ -68,4 +74,9 @@ extinct_animals = {
 # Remove them from extinct_animals and return the key value pair as a two item array.
 # Find the built-in method that helps you accomplish this in the Ruby documentation
 # for Hashes.
-# ----
+
+# passenger_pigeon = extinct_animals.keys[4], extinct_animals.values[4]
+# extinct_animals.delete("Passenger Pigeon")
+# p extinct_animals
+# p passenger_pigeon
+
