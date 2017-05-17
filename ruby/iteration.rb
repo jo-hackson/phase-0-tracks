19bf72dca89fdae3b43f1f7db6e7c5e967601309
jo-hackson <joanna.yeh.jackson@gmail.com>
2017-05-17 13:19:43 +0100

    Complete release 2

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -39,22 +39,63 @@
 # 	# puts fruit + "s"
 # end
 
-maturity = {
-	"apple" => "unripe",
-	"orange" => "unripe",
-	"banana" => "unripe",
-	"plum" => "unripe",
-	"pineapple" => "unripe"
+# maturity = {
+# 	"apple" => "unripe",
+# 	"orange" => "unripe",
+# 	"banana" => "unripe",
+# 	"plum" => "unripe",
+# 	"pineapple" => "unripe"
+# }
+
+# maturity.each do |fruit, ripeness|
+# 	puts "The #{fruit} is #{ripeness}."
+# end
+
+# maturity.map do |fruit, ripeness|
+# 	ripeness = "ripe"
+# 	puts "Now the #{fruit} is #{ripeness}."
+# end
+
+# Release 2
+
+
+# 2. A method that filters a data structure for only items that 
+# do satisfy a certain condition (for example, keeping any numbers that are less than 5).
+# 3. A different method that filters a data structure for only items 
+# satisfying a certain condition -- Ruby offers several options!
+# 4. A method that will remove items from a data structure until the condition 
+# in the block evaluates to false, then stops (you may not find a perfectly working 
+# 	option for the hash, and that's okay).
+
+array_of_numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+hash_of_numbers = {
+	0 => "zero",
+	1 => "one",
+	2 => "two",
+	3 => "three",
+	4 => "four",
+	5 => "five",
+	6 => "six",
+	7 => "seven",
+	8 => "eight",
+	9 => "nine",
+	10 => "ten"
 }
 
-maturity.each do |fruit, ripeness|
-	puts "The #{fruit} is #{ripeness}."
-end
+# 1. A method that iterates through the items, deleting any that 
+# meet a certain condition (for example, deleting any numbers that are less than 5).
 
-maturity.map do |fruit, ripeness|
-	ripeness = "ripe"
-	puts "Now the #{fruit} is #{ripeness}."
-end
+array_of_numbers.delete_if { |number| number == even}
+p array_of_numbers
+
+
+
+
+
+
+
+
 
 
 
