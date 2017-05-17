45b829854cf2ca56b663d0dd7a6e2170240d9b14
jo-hackson <joanna.yeh.jackson@gmail.com>
2017-05-17 13:11:10 +0100

    Complete release 2

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,14 +1,63 @@
-# In phase-0-tracks/ruby/iteration.rb, write your own 
-# method that takes a block. Your method should print 
-# out a status message before and after running the block. 
-# Your block doesn't have to do anything fancy -- it can 
-# just print out several of its own parameters.
+# Release 0
 
-def favorite_food
-	# food1 = "fish"
-	# food2 = "beer"
-	puts "What is your favorite food?"
-	yield("fish", "beer")
+# # first way:
+# def favorite_food
+# 	food1 = "fish"
+# 	food2 = "beer"
+# 	puts "What is your favorite food?"
+# 	yield(food1, food2)
+# end
+
+# # second way:
+# favorite_food { |food1, food2| puts "I like #{food1} and #{food2}!"}
+
+# def favorite_food
+# 	puts "What is your favorite food?"
+# 	yield("fish", "beer")
+# end
+
+# favorite_food { |food1, food2| puts "I like #{food1} and #{food2}!"}
+
+# Release 1
+# Demonstrate that you can iterate through each one using 
+# .each, and then using .map! (modifying the data in some way). 
+# Note that you can't call .map! on a hash, so you can skip that 
+# (.map returns an array, so a destructive map method is logically 
+# impossible on a hash). Print the data structures before and after 
+# each call to demonstrate whether they have been modified or not.
+
+# fruit = ["apple", "orange", "banana", "plum", "pineapple"]
+
+
+# fruit.each do |fruit| 
+# 	puts fruit
+# end
+
+# fruit.map! do |fruit| 
+# 	last_letter = fruit.length
+# 	p fruit.insert(last_letter, "s")
+# 	# puts fruit + "s"
+# end
+
+maturity = {
+	"apple" => "unripe",
+	"orange" => "unripe",
+	"banana" => "unripe",
+	"plum" => "unripe",
+	"pineapple" => "unripe"
+}
+
+maturity.each do |fruit, ripeness|
+	puts "The #{fruit} is #{ripeness}."
 end
 
-favorite_food { |food1, food2| puts "I like #{food1} and #{food2}!"}
+maturity.map do |fruit, ripeness|
+	ripeness = "ripe"
+	puts "Now the #{fruit} is #{ripeness}."
+end
+
+
+
+
+
+
