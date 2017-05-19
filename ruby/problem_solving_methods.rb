ea23d95683fa97a24d6be157230ed9251375d6fe
jo-hackson <joanna.yeh.jackson@gmail.com>
2017-05-19 17:48:44 +0100

    Complete release 2 of 5.6 peer pairing

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -40,38 +40,102 @@
 # create an array with a method of generating and
 # returning Fib terms
 
-def fib(number)
+# def fib(number)
 
-array = [0, 1]
+# array = [0, 1]
 
-# number create that many elements in the array
+# # number create that many elements in the array
 
-# return last number (-1) in the array
-# build code that makes the arrays smaller if number == 0 or 1
+# # return last number (-1) in the array
+# # build code that makes the arrays smaller if number == 0 or 1
 
-if number == 0
-	array = []
-elsif number == 1
-	array = [0]
-else
-	nil
-end
+# if number == 0
+# 	array = []
+# elsif number == 1
+# 	array = [0]
+# else
+# 	nil
+# end
 
-number -= 2
+# number -= 2
 
-number.times { |number| array << (array[-1] + array[-2])}
+# number.times { |number| array << (array[-1] + array[-2])}
 
+# p array
+# p array[-3] + array[-2] == array[-1] # to check work
+# end
 
+# fib(100)
 
-p array
-end
-
-fib(0)
+# fib(0)
 # fib(0) #=> [0, 1] #=> expect []
 # fib(1) #=> [0, 1] #=> expect [0]
 # fib(2) #=> [0, 1] 
 # fib(3) #=> [0, 1, 1] 
 # fib(6)
 # fib(100)
+
+# Release 2
+
+# Goal: write a method that will sort values in ascending order
+# assume all values in array are integers
+
+# Find lowest value and place into a new empty array
+# Loop to until no values remain in the original array
+# Replace old array with new array
+
+array = [36, 8, 3, 567, 24, 734, 57, 4, 16, 86]
+
+def sort(array)
+	new_array = []
+
+	until array == []
+		new_array << array.min
+		array.delete(array.min)
+	end
+	array = new_array	
+end
+
+p sort(array)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
