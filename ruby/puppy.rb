c53a4dab9ffda7f2eae129f3181203c0185c44c6
jo-hackson <joanna.yeh.jackson@gmail.com>
2017-05-22 10:46:55 +0100

    Complete 6.1 with class Puppy attributes

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+# design a puppy class
+# add characteristics and behaviors
+
+class Puppy
+
+species ------------
+llamalookalike
+
+characteristics ------------
+eyelash length: > 5 inches
+neck length: > 2 feet
+tongue: always sticking out
+color: tan, white, or brown
+ears: perky	
+
+behavior ------------
+being adorable
+feature at petting zoos
+giving sloppy kisses
