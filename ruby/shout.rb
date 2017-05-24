47844c9ab904dcbdaf0e3fe8b485576574b17064
jo-hackson <joanna.yeh.jackson@gmail.com>
2017-05-24 09:05:53 +0100

    Add class FootballFans, class IceCreamVendorOnTheBeach. Update module to have a calling_over method which is included for both classes. Add driver code to confirm it works and print phrase.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -2,14 +2,33 @@
 # on the self keyword
 
 module Shout
-	def self.yell_angrily(words)
-    words + "!!!" + " :("
-	end
+	# def self.yell_angrily(words)
+ #    words + "!!!" + " :("
+	# end
 	
-    def self.yelling_happily(words)
-    words + "(:"
+ #    def self.yelling_happily(words)
+ #    words + "(:"
+ # 	end
+
+ 	def calling_over(phrase)
+ 		puts "Come and get #{phrase}"
  	end
+
  end
 
-p Shout.yell_angrily("Boohoo")
-p Shout.yelling_happily("Whoohoo")
+# p Shout.yell_angrily("Boohoo")
+# p Shout.yelling_happily("Whoohoo")
+
+class FootballFans
+	include Shout
+end
+
+class IceCreamVendorOnTheBeach
+	include Shout
+end
+
+footballfan = FootballFans.new
+footballfan.calling_over("us! You won't be able to get past!")
+
+icecreamvendor = IceCreamVendorOnTheBeach.new
+icecreamvendor.calling_over("some cold ice cream!")
