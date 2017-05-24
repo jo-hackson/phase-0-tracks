4449f53566a47c1765a55ec45f2922e006e29497
jo-hackson <joanna.yeh.jackson@gmail.com>
2017-05-24 08:54:25 +0100

    Add class Bird, class Plane, and module Flight

--- ruby/flight.rb
+++ ruby/flight.rb
@@ -1,3 +1,7 @@
+# module goes before class
+# do not use self when module is a mixin
+# write the same way as an instance method inside class
+
 module Flight
   def take_off(altitude)
     puts "Taking off and ascending until reaching #{altitude} ..."
@@ -5,8 +9,15 @@ module Flight
 end
 
 class Bird
+	include flight
 end
 
 class Plane
+	include flight
 end
 
+bird = Bird.new
+bird.take_off(800)
+
+plane = Plane.new
+plane.take_off(30000)
