37c416b256361ca9275843b8533378261dfd18a8
jo-hackson <joanna.yeh.jackson@gmail.com>
2017-05-30 19:35:43 +0100

    Create keyvaluematch function but unable to add items to array

--- js/algos.js
+++ js/algos.js
@@ -19,8 +19,63 @@
 	console.log(input_array)
 }
 
-biggestWord(["long","longest","longer"]) 
+// biggestWord(["long","longest","longer"]) 
 
 // ____________________________________________________________
 
-Release 1: Find a Key-Value Match
+// Release 1: Find a Key-Value Match
+
+// input: two objects
+// goal: check if objects share at least one key-value pair
+// hint: may not have any of the same key
+// steps: check if keys match, if not, break and return false
+	// if keys match, check if those values match, return true
+	// else, return false
+	// special note: == checks only if objects are the same instance,
+	//		would not work for checking if contents are the same
+// output: boolean
+
+
+function keyValueMatch(object1, object2) {
+
+	for (var i = 0; i < Object.keys(object1).length; i++) {
+
+		var tally = [];
+		console.log(tally);
+
+		if (Object.keys(object1)[i] != Object.keys(object2)[i]) {
+			console.log("1");
+			tally.push("false");
+		} else {
+			if (Object.values(object1)[i] == Object.values(object2)[i]) {
+				console.log("2");
+				tally.push("true");
+			} else {
+				console.log("3");
+				tally.push("false");
+			}
+		}
+	}
+
+		if (tally.includes("true")) {
+			console.log("true");
+			return true
+		} else {
+			console.log("false");
+			return false
+		}
+		}
+
+		
+
+// keyValueMatch({name: "Steven", age: 54}, {name: "Tamir", age: 54}); //  true
+keyValueMatch({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3}); //  true
+keyValueMatch({animal: "Cat", legs: 4}, {animal: "Dog", legs: 4}); //  true
+
+
+
+
+
+
+
+
