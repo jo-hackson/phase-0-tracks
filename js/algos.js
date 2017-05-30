6c3fe3de4d5ec89b5f3e541f00a5584f1555f4ad
jo-hackson <joanna.yeh.jackson@gmail.com>
2017-05-30 10:30:48 +0100

    Add algos file

---
+++ js/algos.js
@@ -0,0 +1,28 @@
+write a function that takes an array of words or phrases 
+and returns the longest word or phrase in the array. 
+So if we gave your function the array of 
+["long phrase","longest phrase","longer phrase"], 
+it would return "longest phrase". This is a logical 
+reasoning challenge, so try to reason through this based 
+on what you already know, rather than Googling the fanciest 
+solution or looking up built-in sorting functions. 
+Note that "some string".length will do what you expect in JS.
+
+
+// input: array of words  
+	// hint: use .length
+	// steps: compare two words by their lengths
+	// delete the word that is short
+	// loop through array until length of array = 1
+	// then print on console the array in string form
+// output: returns the longest word in the array
+
+function biggestWord(word_array_input){
+
+
+
+
+} // function
+
+
+biggestWord([["long","longest","longer"]]) // expected output "longest"
