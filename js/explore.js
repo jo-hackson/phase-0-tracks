8c90b3901d10f19f45e3e882066579ab34fd0a8c
jo-hackson <joanna.yeh.jackson@gmail.com>
2017-05-30 10:21:18 +0100

    Delete instructions from challenge that were copied and pasted into the code

--- js/explore.js
+++ js/explore.js
@@ -1,14 +1,3 @@
-// Ruby has lots of handy functions like .reverse, 
-// but in JavaScript, we mostly have to write our own. 
-// In comments, pseudocode a function that takes a 
-// string as a parameter and reverses the string. reverse("hello"), 
-// for example, should return "olleh". This isn't a task specific 
-// to JavaScript -- your pseudocode should be in plain English and 
-// use phrases like "for each" instead of JavaScript-specific terms. 
-// Remember that you can add strings in JavaScript, so '' + 'a' would 
-// result in the string 'a'. Take your time. This is the most valuable 
-// skill in programming: being able to think through a problem logically.
-
 // input: string
 // steps: create an empty string and to add letters in descending order to
 	// reminder: java is also 0-indexed
