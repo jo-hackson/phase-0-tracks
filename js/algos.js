c3e5d52612d2124b04ec59dfa2195883a3dc1165
jo-hackson <joanna.yeh.jackson@gmail.com>
2017-05-30 11:34:27 +0100

    Complete biggest word method to return largest word in inputted array

--- js/algos.js
+++ js/algos.js
@@ -1,14 +1,4 @@
-// write a function that takes an array of words or phrases 
-// and returns the longest word or phrase in the array. 
-// So if we gave your function the array of 
-// ["long phrase","longest phrase","longer phrase"], 
-// it would return "longest phrase". This is a logical 
-// reasoning challenge, so try to reason through this based 
-// on what you already know, rather than Googling the fanciest 
-// solution or looking up built-in sorting functions. 
-// Note that "some string".length will do what you expect in JS.
-
-
+// Release 0: Find the Longest Phrase
 // input: array of words  
 	// hint: use .length
 	// steps: compare two words by their lengths and delete the word that is shorter
@@ -18,20 +8,19 @@
 
 // function biggestWord(input_array){
 	function biggestWord(input_array) {
-	
-	i = 0
-	if (input_array[i].length > input_array[i+1].length) {
-		input_array.splice(i, i+1)
+
+	while (input_array.length!= 1) {
+	if (input_array[0].length > input_array[1].length) {
+		input_array.splice(1, 1)
 	} else {
-		input_array.splice(i, 1)
+		input_array.splice(0, 1)
+	} 
 	}
-
 	console.log(input_array)
+}
 
-	
+biggestWord(["long","longest","longer"]) 
 
-} // function
+// ____________________________________________________________
 
-
-biggestWord(["long","longest","longer"]) // expected output "longest"
-
+Release 1: Find a Key-Value Match
