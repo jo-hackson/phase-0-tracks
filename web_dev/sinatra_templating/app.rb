3031e822e3e52ef3051c4eeeb17d8b74e687471f
jo-hackson <joanna.yeh.jackson@gmail.com>
2017-06-14 12:20:41 -0400

    Complete 9.5 challenge

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -7,8 +7,17 @@ set :public_folder, File.dirname(__FILE__) + '/static'
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
+create_campuses_table = <<-SQL
+	CREATE TABLE IF NOT EXISTS campuses(
+		id INTEGER PRIMARY KEY,
+		campus_city VARCHAR(255),
+		FOREIGN KEY (campus_city) REFERENCES students(campus)
+	);
+SQL
+
 # show students on the home page
 get '/' do
+  db.execute(create_campuses_table)
   @students = db.execute("SELECT * FROM students")
   erb :home
 end
@@ -17,10 +26,23 @@ get '/students/new' do
   erb :new_student
 end
 
+get '/students/age' do
+  erb :age_ranking
+end
+
+get '/campuses/new' do
+	erb :new_campus
+end
+
 # create new students via
 # a form
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
+  redirect '/'
+end
+
+post '/campuses' do 
+  db.execute("INSERT INTO campuses (campus_city) VALUES (?)", [params['campus_city']])
   redirect '/'
 end
 
